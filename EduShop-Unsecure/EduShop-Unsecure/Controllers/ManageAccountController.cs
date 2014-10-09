60feb5b997156c0b0f54a5e63f93c611eee82088
AndreasElffors <Andreas.elffors@gmail.com>
2014-10-09 22:39:42 +0200

    Implemented Logout & Part of Login

--- EduShop-Unsecure/EduShop-Unsecure/Controllers/ManageAccountController.cs
+++ EduShop-Unsecure/EduShop-Unsecure/Controllers/ManageAccountController.cs
@@ -17,18 +17,37 @@ namespace EduShop_Unsecure.Controllers
             return PartialView("_Login", new UserModel());
         }
 
-        [ChildActionOnly]
+
         [HttpPost]
         public ActionResult Login(UserModel model)
         {
+
+            if (model.Email == "Test@test.com" && model.Password == "12345678")
+            {
+                HttpCookie authentication = new HttpCookie("Auth");
+                authentication.Value = model.Email;
+                Response.Cookies.Add(authentication);
+                return RedirectToAction("Index", "Home");
+                //return PartialView("_Login");
+            }
+            return RedirectToAction("Index", "Home");
             //var userModel = new UserModel();//todo....
             //var user = new User();
 
             //user = UserModel.CheckForUser(model);
 
             //userModel = UserModel.ConvertToUserModel(user);
+        }
 
-            return PartialView("_Login");
+        public ActionResult LogOut()
+        {
+            if (Request.Cookies["Auth"] != null)
+            {
+                HttpCookie authentication = new HttpCookie("Auth");
+                authentication.Expires = DateTime.Now.AddDays(-1d);
+                Response.Cookies.Add(authentication);
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         public ActionResult Register()
